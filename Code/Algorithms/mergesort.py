32e874fa74591fc4c43934a6ed0d9f63f25685a3
abhishektiwary17 <115434559+abhishektiwary17@users.noreply.github.com>
2022-10-12 03:36:28 +0530

    Create mergesort.py

---
+++ Code/Algorithms/mergesort.py
@@ -0,0 +1,45 @@
+#Code to implement merge sort
+
+def ms(arr):
+    if len(arr)>1:
+        mid=len(arr)//2   #finding the mid of the array and dividing it into 2 parts
+        l=arr[:mid]
+        r=arr[mid:]
+        ms(l)
+        ms(r)
+        i=j=k=0
+        while i<len(l) and j<len(r):
+            if l[i]<=r[j]:
+                arr[k]=l[i]
+                i=i+1
+            else:
+                arr[k]=r[j]
+                j=j+1
+            k=k+1
+        while i<len(l):
+            arr[k]=l[i]
+            i=i+1
+            k=k+1
+        while j<len(r):
+            arr[k]=r[j]
+            j=j+1
+            k=k+1
+
+def pl(arr):
+    for i in range(len(arr)):
+        print(arr[i],end=" ")
+    print()
+
+
+arr=[]
+y=int(input("Enter the number of elements:"))
+for a in range(0,y):
+    b=int(input())
+    arr.append(b)
+
+
+print("Given array:",end="\n")
+pl(arr)
+ms(arr)
+print("Sorted array:",end="\n")
+pl(arr)
