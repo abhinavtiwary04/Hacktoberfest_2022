cd214dce35f710d4041c8dac1a3ffdd6b651b827
abhishektiwary17 <115434559+abhishektiwary17@users.noreply.github.com>
2022-10-14 00:18:08 +0530

    Update bubblesort.py
    Fixed minor bugs.

--- Code/Algorithms/bubblesort.py
+++ Code/Algorithms/bubblesort.py
@@ -6,12 +6,15 @@ def bs(arr):
         for j in range(0, l-i-1):
             if arr[j]>arr[j+1]:
                 arr[j], arr[j+1]=arr[j+1],arr[j]   #Swapping the elements
-arr=[]
-y=int(input("Enter the number of elements:"))
-for a in range(0,y):
-    b=int(input())
-    arr.append(b)
-bs(arr)
-print("Sorted array:")
-for i in range(len(arr)):
-    print("%d" % arr[i], end=" ")
+if __name__=="__main__":
+    arr=[]
+    y=int(input("Enter the number of elements:"))
+    if(y!=0):
+        print("Enter the numbers:")
+    for a in range(0,y):
+        b=int(input())
+        arr.append(b)
+    bs(arr)
+    print("Sorted array:")
+    for i in range(len(arr)):
+        print("%d" % arr[i], end=" ")
